7eaa15b1fd3a429f8a997904cf42f526150f951f
RICHARDAAH <richardhayim@hotmail.com>
2017-01-13 12:05:11 +0000

    inserting footer

--- db/seeds.js
+++ db/seeds.js
@@ -7,7 +7,7 @@ const Gallery      = require('../models/gallery');
 
 Gallery.collection.drop();
 
-const gallery     = new Gallery({
+const gallery1     = new Gallery({
   name: 'Whitechapel Gallery',
   string: 'Whitechapel+Gallery',
   img: 'images/whitechapel.jpg',
@@ -16,21 +16,21 @@ const gallery     = new Gallery({
 });
 
 gallery1.save((err, gallery) => {
- if(err) return console.log(err);
- return console.log(`${gallery.name} was saved`);
+  if(err) return console.log(err);
+  return console.log(`${gallery.name} was saved`);
 });
 
 const gallery2     = new Gallery({
   name: 'Lawrence Alkin Galley',
   string: 'Lawrence+Alkin+Gallery',
-  img:  'images/lawrencealkin.jpg',
+  img: 'images/lawrencealkin.jpg',
   lat: '51.5154621',
   lng: '-0.1450903'
 });
 
 gallery2.save((err, gallery) => {
- if(err) return console.log(err);
- return console.log(`${gallery.name} was saved`);
+  if(err) return console.log(err);
+  return console.log(`${gallery.name} was saved`);
 });
 
 const gallery3     = new Gallery({
@@ -42,45 +42,45 @@ const gallery3     = new Gallery({
 });
 
 gallery3.save((err, gallery) => {
- if(err) return console.log(err);
- return console.log(`${gallery.name} was saved`);
+  if(err) return console.log(err);
+  return console.log(`${gallery.name} was saved`);
 });
 
 const gallery4     = new Gallery({
   name: 'Eames Fine Art Gallery',
   string: 'Eames+Fine+Art+Gallery',
-  img:  'images/eamesfineart.jpg',
+  img: 'images/eamesfineart.jpg',
   lat: '51.5017655',
   lng: '-0.0999106'
 });
 
 gallery4.save((err, gallery) => {
- if(err) return console.log(err);
- return console.log(`${gallery.name} was saved`);
+  if(err) return console.log(err);
+  return console.log(`${gallery.name} was saved`);
 });
 
 const gallery5     = new Gallery({
   name: 'Alan Cristia Gallery',
   string: 'Alan+Cristia+Gallery',
-  img:  'images/alancristia.jpg',
+  img: 'images/alancristia.jpg',
   lat: '51.5153338',
   lng: '-0.1451775'
 });
 
 gallery5.save((err, gallery) => {
- if(err) return console.log(err);
- return console.log(`${gallery.name} was saved`);
+  if(err) return console.log(err);
+  return console.log(`${gallery.name} was saved`);
 });
 
 const gallery6     = new Gallery({
-  name: 'Castle Fine Art'
+  name: 'Castle Fine Art',
   string: 'Castle+Fine+Art',
-  img:   'castlefineart.jpg',
+  img: 'images/castlefineart.jpg',
   lat: '51.5154595',
   lng: '-0.1450903'
 });
 
 gallery6.save((err, gallery) => {
- if(err) return console.log(err);
- return console.log(`${gallery.name} was saved`);
+  if(err) return console.log(err);
+  return console.log(`${gallery.name} was saved`);
 });
