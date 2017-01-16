ad564458330d9c09de8da78c06d9713f67199399
RICHARDAAH <richardhayim@hotmail.com>
2017-01-16 17:27:41 +0000

    End of Monday

--- src/js/app.js
+++ src/js/app.js
@@ -1,83 +1,122 @@
-// const googleMap = googleMap || {};
-// const google = google;
-//
-// googleMap.addInfoWindowForGallery = function(gallery, marker) {
-//   google.maps.event.addListener(marker, 'click', () => {
-//     if (typeof this.infoWindow !== 'undefined') this.infoWindow.close();
-//
-//     this.infoWindow = new google.maps.InfoWindow({
-//       content: `<img src="../${gallery.img}"><p>${ gallery.name }</p>`
-//     });
-//
-//     this.infoWindow.open(this.map, marker);
-//     this.map.setCenter(marker.getPosition());
-//     this.map.setZoom(15);
-//   });
-// };
-//
-// googleMap.createMarkerForGallery = function(gallery) {
-//   const latlng = new google.maps.LatLng(gallery.lat, gallery.lng);
-//   const marker = new google.maps.Marker({
-//     position: latlng,
-//     map: this.map,
-//     animation: google.maps.Animation.DROP
-//   });
-//
-//   this.addInfoWindowForGallery(gallery, marker);
-// };
-//
-// googleMap.loopThroughGalleries = function(data) {
-//   $.each(data.galleries, (index, gallery) => {
-//     setTimeout(() => {
-//       console.log(gallery);
-//       googleMap.createMarkerForGallery(gallery);
-//     }, index * 50);
-//   });
-// };
-//
-// googleMap.getGalleries = function() {
-//   $.get('http://localhost:3000/api/galleries').done(this.loopThroughGalleries);
-// };
-//
-// googleMap.mapSetup = function() {
-//   const canvas = document.getElementById('map-canvas');
-//
-//   const mapOptions = {
-//     zoom: 12,
-//     center: new google.maps.LatLng(51.506178,-0.088369),
-//     mapTypeId: google.maps.MapTypeId.ROADMAP
-//   };
-//
-//   this.map = new google.maps.Map(canvas, mapOptions);
-//   this.getGalleries();
-// };
+$(start);
+
+var map;
+var infoWindow;
+var google = window.google;
 
 function start() {
+  $('#login').on('submit', login);
+  $('#signup').on('submit', register);
+  $('.logout').on('click', logout);
 
-  // googleMap.mapSetup.bind(googleMap)();
+  if (getToken()){
+    loggedInState();
+  } else {
+    loggedOutState();
+  }
+}
 
-  $('#login').on('submit', function(e){
-    e.preventDefault();
-    $.ajax({
-      method: 'POST',
-      url: 'http://localhost:3000/api/login',
-      data: $(this).serialize()
-    }).done(data => {
-      console.log(data);
-    });
-  });
+function loggedInState() {
+  $('.loggedOut').hide();
+  $('.loggedIn').show();
+  mapSetup();
+}
 
-  $('#signup').on('submit', function(e){
-    e.preventDefault();
-    console.log('#signup');
-    $.ajax({
-      method: 'POST',
-      url: 'http://localhost:3000/api/register',
-      data: $(this).serialize()
-    }).done(data => {
-      console.log(data);
-    });
+function loggedOutState() {
+  $('.loggedIn').hide();
+  $('.loggedOut').show();
+  $('#map-canvas').empty();
+}
+
+function login(e){
+  e.preventDefault();
+
+  $.ajax({
+    method: 'POST',
+    url: 'http://localhost:3000/api/login',
+    data: $(this).serialize()
+  }).done(getLoggedIn);
+}
+
+function register(e){
+  e.preventDefault();
+
+  $.ajax({
+    method: 'POST',
+    url: 'http://localhost:3000/api/register',
+    data: $(this).serialize()
+  }).done(getLoggedIn);
+}
+
+function logout(e) {
+  e.preventDefault();
+  removeToken();
+  loggedOutState();
+}
+
+function getLoggedIn(data) {
+  if (data.token) {
+    setToken(data.token);
+    loggedInState();
+  }
+}
+
+function setToken(token){
+  return window.localStorage.setItem('token', token);
+}
+
+function getToken(){
+  return window.localStorage.getItem('token');
+}
+
+function removeToken(){
+  return window.localStorage.clear();
+}
+
+function mapSetup() {
+  const canvas = document.getElementById('map-canvas');
+
+  const mapOptions = {
+    zoom: 12,
+    center: new google.maps.LatLng(51.506178,-0.088369),
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+
+  map = new google.maps.Map(canvas, mapOptions);
+  getGalleries();
+}
+
+function getGalleries() {
+  $.get('http://localhost:3000/api/galleries').done(loopThroughGalleries);
+}
+
+function loopThroughGalleries(data) {
+  $.each(data.galleries, (index, gallery) => {
+    createMarkerForGallery(gallery);
   });
 }
 
-$(start);
+function createMarkerForGallery(gallery) {
+  const latlng = new google.maps.LatLng(gallery.lat, gallery.lng);
+  const marker = new google.maps.Marker({
+    position: latlng,
+    map: map,
+    animation: google.maps.Animation.DROP
+  });
+
+  addInfoWindowForGallery(gallery, marker);
+}
+
+function addInfoWindowForGallery(gallery, marker) {
+  google.maps.event.addListener(marker, 'click', () => {
+    if (typeof infoWindow !== 'undefined') infoWindow.close();
+
+    infoWindow = new google.maps.InfoWindow({
+      content: `<img src="../${gallery.img}"><p>${ gallery.name }</p>`
+    });
+
+    infoWindow.open(map, marker);
+    map.setCenter(marker.getPosition());
+    map.setZoom(15);
+  });
+}
